bd68acb8c06cf54334b1ea97f1f9a565d9d6b0ed
DmitriyR54 <45ryirtimd@gmail.com>
2022-12-23 00:48:26 +0200

    intro section update

--- aquarium/src/pages/Home/homeSections/Intro/Intro.js
+++ aquarium/src/pages/Home/homeSections/Intro/Intro.js
@@ -1,9 +1,43 @@
+// libraries
 import Plyr from 'plyr';
 import 'plyr/dist/plyr.css';
+// styles
 import './Intro.scss';
 
 const IntroSection = () => {
-    const playVideoBtn = document.querySelector('.intro__play-btn');
+    const intro = document.querySelector('.intro');
+
+    /* smooth appearing on the screen */
+    const introTitle = intro.querySelector('.intro__title');
+    const introSubtitle = intro.querySelector('.intro__subtitle');
+    const introLink = intro.querySelector('.intro__link');
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // show the items
+                introTitle.style.opacity = '1';
+                introTitle.style.transform = 'translate3d(0, 0, 0)';
+                introSubtitle.style.opacity = '1';
+                introSubtitle.style.transform = 'translate3d(0, 0, 0)';
+                introLink.style.opacity = '1';
+                introLink.style.transform = 'translate3d(0, 0, 0)';
+
+                observer.unobserve(entry.target);
+            }
+        });
+    };
+
+    const observerOptions = {
+        threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    observer.observe(intro);
+
+    /* video player */
+    const playVideoBtn = intro.querySelector('.intro__play-btn');
     const player = new Plyr('#intro__player');
 
     function playVideo() {
